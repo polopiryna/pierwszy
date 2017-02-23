9c6fbcf9f2f6cbe6a911e56822495837f7956b7f
polopiryna <hcx@wp.pl>
2017-02-24 00:08:19 +0100

    Add files via upload

---
+++ pierwszy.py
@@ -0,0 +1,9 @@
+print   ("witaj kolego") # to jest komentarz i prosta instrukcja print#
+                         # ta instrukcja wyswietli napis Witaj Kolego #
+input   ("\n\nAby zakonczyc program, nacisnij Enter.")
+        #instrukcja input odpowiedzialna jest na reakcje ze strony użytkownika#
+
+
+
+
+       
